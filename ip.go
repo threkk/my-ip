420e061993e1b7c1bcbf5ef79c32e8f8d907026c
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2018-01-15 02:58:48 +0100

    Updated external and internal functions. Soon ready to go live!

--- ip.go
+++ ip.go
@@ -6,42 +6,64 @@ import (
 	"net"
 )
 
-// OpenDNS1 - Resolver 1 of OpenDNS.
-const OpenDNS1 = "208.67.222.222:53"
+const (
+	// OpenDNS1 Main resolver address of OpenDNS.
+	OpenDNS1 = "208.67.222.222:53"
 
-// OpenDNS2 - Resolver 2 of OpenDNS.
-const OpenDNS2 = "208.67.220.220:53"
+	// OpenDNS2 Secondary resolver address of OpenDNS.
+	OpenDNS2 = "208.67.220.220:53"
 
-// GoogleDNS1 - Resolver 1 of Google.
-const GoogleDNS1 = "8.8.8.8:53"
+	// MyIP Route to resolve in order to get the external ip address.
+	MyIP = "myip.opendns.com"
+)
 
-// GoogleDNS2 - Resolver 2 of Google.
-const GoogleDNS2 = "8.8.4.4:53"
+var (
+	// PreferIP6  If given the option, it should return the ipv6 over ipv4.
+	PreferIP6 = false
+)
 
-// MyIP TODO
-type MyIP struct {
-	DNS       string
-	PreferIP6 bool
-	Ctx       context.Context
+// openDNS1Dialer Dialer pointing to the DNS1 from OpenDNS.
+func openDNS1Dialer(ctx context.Context, network, address string) (net.Conn, error) {
+	d := net.Dialer{}
+	return d.DialContext(ctx, "udp", OpenDNS1)
 }
 
-// https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go#23558495
+// openDNS2Dialer Dialer pointing to the DNS2 from OpenDNS.
+func openDNS2Dialer(ctx context.Context, network, address string) (net.Conn, error) {
+	d := net.Dialer{}
+	return d.DialContext(ctx, "udp", OpenDNS2)
+}
 
-// External TODO
-func (myIP *MyIP) External() (string, error) {
-	dialer := net.Dialer{}
-	d, err := dialer.DialContext(myIP.Ctx, "udp", myIP.DNS)
-	defer d.Close()
-	if err != nil {
-		return "", err
+// External Retrieves the external IP address for the host by dialing to the
+// OpenDNS DNS server. Upon resolving the ip address, it will return the sender's
+// ip address.
+func External() ([]string, error) {
+	externals := make([]string, 0)
+	ctxbg := context.Background()
+	resolver := &net.Resolver{
+		Dial:     openDNS1Dialer,
+		PreferGo: true,
 	}
 
-	address := d.LocalAddr().String()
-	return address, nil
+	ips, err := resolver.LookupIPAddr(ctxbg, "myip.opendns.com")
+	if err != nil {
+		resolver.Dial = openDNS2Dialer
+		ips, err = resolver.LookupIPAddr(ctxbg, "myip.opendns.com")
+
+		if err != nil {
+			return externals, err
+		}
+	}
+
+	for _, ip := range ips {
+		externals = append(externals, ip.String())
+	}
+
+	return externals, nil
 }
 
 // Internal TODO
-func (myIP *MyIP) Internal() ([]string, error) {
+func Internal() ([]string, error) {
 	ifaces, err := net.InterfaceAddrs()
 
 	if err != nil {
@@ -50,13 +72,15 @@ func (myIP *MyIP) Internal() ([]string, error) {
 
 	locals := make([]string, 0)
 	for _, address := range ifaces {
-		if ip, ok := address.(*net.IPNet); ok && !ip.IP.IsLoopback() {
-			if ip4 := ip.IP.To4(); ip4 != nil {
-				locals = append(locals, ip4.String())
-			}
+		ip, ok := address.(*net.IPNet)
+		if ok && (!ip.IP.IsLoopback()) {
+			ip4 := ip.IP.To4()
+			ip6 := ip.IP.To16()
 
-			if ip6 := ip.IP.To16(); ip6 != nil {
+			if PreferIP6 && ip6 != nil {
 				locals = append(locals, ip6.String())
+			} else if ip4 != nil {
+				locals = append(locals, ip4.String())
 			}
 		}
 	}
@@ -64,18 +88,10 @@ func (myIP *MyIP) Internal() ([]string, error) {
 	return locals, nil
 }
 
-func openDNSDialer(ctx context.Context, network, address string) (net.Conn, error) {
-	d := net.Dialer{}
-	// OpenDNS server ip address.
-	return d.DialContext(ctx, "udp", "")
-}
-
 func main() {
-	myIP := &MyIP{DNS: OpenDNS1, Ctx: context.Background(), PreferIP6: false}
-
-	ext, _ := myIP.External()
-	inte, _ := myIP.Internal()
-
+	PreferIP6 = true
+	ext, _ := External()
+	inte, _ := Internal()
 	fmt.Println("External", ext)
 	fmt.Println("Internal", inte)
 }
